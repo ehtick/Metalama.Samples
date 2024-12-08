fd201aab428d72791f63cebac098663fb707ac22
GaelFraiteur <gael@postsharp.net>
2024-12-08 14:02:16 +0100

    Fixing tests.

--- examples/metrics/Metalama.Samples.Metrics.Tests/HatShop.HatShopMetrics.i.cs
+++ examples/metrics/Metalama.Samples.Metrics.Tests/HatShop.HatShopMetrics.i.cs
@@ -9,19 +9,19 @@ namespace Metrics
     internal readonly Counter<long> PlaceOrderExceptionCount;
     internal readonly Counter<long> PlaceOrderExecutionCount;
     internal readonly Counter<long> PlaceOrderExecutionTime;
-    private IMeterFactory meterFactory;
-    private IMetricHost metricHost;
+    private IMeterFactory _meterFactory;
+    private IMetricHost _metricHost;
     public HatShopMetrics(IMeterFactory? meterFactory = null, IMetricHost? metricHost = null)
     {
-      this.meterFactory = meterFactory ?? throw new System.ArgumentNullException(nameof(meterFactory));
-      this.metricHost = metricHost ?? throw new System.ArgumentNullException(nameof(metricHost));
-      var meter = this.meterFactory.Create(this.metricHost.ApplicationName, this.metricHost.ApplicationVersion, this.metricHost.Tags);
+      this._meterFactory = meterFactory ?? throw new System.ArgumentNullException(nameof(meterFactory));
+      this._metricHost = metricHost ?? throw new System.ArgumentNullException(nameof(metricHost));
+      var meter = _meterFactory.Create(_metricHost.ApplicationName, _metricHost.ApplicationVersion, _metricHost.Tags);
       PlaceOrderExceptionCount = meter.CreateCounter<long>("PlaceOrder.ExceptionCount");
-      this.metricHost.RegisterInstrument(PlaceOrderExceptionCount, typeof(HatShop).GetMethod("PlaceOrder", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null), "ExceptionCount");
+      _metricHost.RegisterInstrument(PlaceOrderExceptionCount, typeof(HatShop).GetMethod("PlaceOrder", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null), "ExceptionCount");
       PlaceOrderExecutionCount = meter.CreateCounter<long>("PlaceOrder.ExecutionCount");
-      this.metricHost.RegisterInstrument(PlaceOrderExecutionCount, typeof(HatShop).GetMethod("PlaceOrder", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null), "ExecutionCount");
+      _metricHost.RegisterInstrument(PlaceOrderExecutionCount, typeof(HatShop).GetMethod("PlaceOrder", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null), "ExecutionCount");
       PlaceOrderExecutionTime = meter.CreateCounter<long>("PlaceOrder.ExecutionTime");
-      this.metricHost.RegisterInstrument(PlaceOrderExecutionTime, typeof(HatShop).GetMethod("PlaceOrder", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null), "ExecutionTime");
+      _metricHost.RegisterInstrument(PlaceOrderExecutionTime, typeof(HatShop).GetMethod("PlaceOrder", BindingFlags.Public | BindingFlags.Instance, null, Type.EmptyTypes, null), "ExecutionTime");
     }
   }
 }
